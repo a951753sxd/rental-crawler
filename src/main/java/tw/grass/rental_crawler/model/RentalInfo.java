707816468ddf54748dddb8ad811ba7f1f7ab80c1
a951753sxd <a363636005@gmail.com>
2024-08-26 14:05:42 +0800

    java爬蟲挑戰 Day 8 - 重構小部分程式碼.

--- src/main/java/tw/grass/rental_crawler/model/RentalInfo.java
+++ src/main/java/tw/grass/rental_crawler/model/RentalInfo.java
@@ -3,7 +3,7 @@ package tw.grass.rental_crawler.model;
 import lombok.Data;
 
 @Data
-public class RentalListing {
+public class RentalInfo {
     private String title;
     private String link;
     private String address;
--- src/main/java/tw/grass/rental_crawler/service/impl/RentalCrawlerServiceImpl.java
+++ src/main/java/tw/grass/rental_crawler/service/impl/RentalCrawlerServiceImpl.java
@@ -1,6 +1,8 @@
 package tw.grass.rental_crawler.service.impl;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,7 +16,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import tw.grass.rental_crawler.model.RentalListing;
+import tw.grass.rental_crawler.model.RentalInfo;
 import tw.grass.rental_crawler.service.RentalCrawlerService;
 
 @Service
@@ -32,6 +34,27 @@ public class RentalCrawlerServiceImpl implements RentalCrawlerService {
     public void fetchRentalData() {
         log.info("Starting to fetch rental data...");
 
+        WebDriver driver = webDriverInit();
+
+        try {
+            //這邊使用591的條件other=newPost:新上架、sort=posttime_desc:排序為新到舊
+            String urlString = "https://rent.591.com.tw/list?other=newPost&sort=posttime_desc";
+            driver.get(urlString);
+            
+            String pageSource = driver.getPageSource();
+            Document doc = Jsoup.parse(pageSource);
+            
+            parseHTML(doc);
+            log.info("Successfully fetched rental data");
+        } catch (Exception e) {
+            log.error("Error while fetching rental data", e);
+        } finally {
+            driver.quit();
+        }
+    }
+
+
+    private WebDriver webDriverInit() {
         // 設定 ChromeDriver 路徑
         System.setProperty(webdriverName, webdriverPath);
 
@@ -42,68 +65,32 @@ public class RentalCrawlerServiceImpl implements RentalCrawlerService {
         options.addArguments("--window-size=1920,1080"); // 設置窗口大小
 
         WebDriver driver = new ChromeDriver(options);
-        try {
-            //這邊使用591的條件other=newPost:新上架、sort=posttime_desc:排序為新到舊
-            String urlString = "https://rent.591.com.tw/list?other=newPost&sort=posttime_desc";
-            //使用webDriver前往該網址
-            driver.get(urlString);
-            
-            // 等待 JavaScript 加載完畢，最多等10秒
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-            // 取得網頁的 HTML
-            String pageSource = driver.getPageSource();
-
-            // 使用 Jsoup 解析 HTML
-            Document doc = Jsoup.parse(pageSource);
-            
-            // 在這裡解析HTML並提取所需數據
-            parseHTML(doc);
-            log.info("Successfully fetched rental data");
-
-        } catch (Exception e) {
-            log.error("Error while fetching rental data", e);
-        } finally {
-            // 關閉 WebDriver
-            driver.quit();
-        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
     }
 
-    private void parseHTML(Document doc) {
-        Elements listings = doc.select("div.item-info");
 
-        // 迴圈遍歷每個房源資訊
+    private void parseHTML(Document doc) {
+        List<RentalInfo> list = new ArrayList<RentalInfo>();
+        
+        Elements listings = doc.select("div.item-info");
         for (Element listing : listings) {
-            // 提取標題
             String title = listing.select("a.link").attr("title");
-            // 提取連結
             String link = listing.select("a.link").attr("href");
-            // 提取地址
             String address = listing.select("div.item-info-txt i.house-place").next().text();
-            // 提取價格
             String price = listing.select("div.item-info-price strong").text();
-            // 提取樓層與坪數
             String floorAndArea = listing.select("div.item-info-txt").get(1).text();
-            // 提取距離捷運
             String distanceToMrtName = listing.select("div.item-info-txt i.house-metro").next().text();
             String distanceToMRT = listing.select("div.item-info-txt i.house-metro").next().next().text();
 
-            RentalListing rentalList = new RentalListing();
-            rentalList.setTitle(title);
-            rentalList.setLink(link);
-            rentalList.setAddress(address);
-            rentalList.setPrice(price);
-            rentalList.setFloorAndArea(title);
-            rentalList.setDistanceToMRT(title);
-            
-            // 輸出房源資訊
-            log.info("Title: {}", title);
-            log.info("Link: {}", link);
-            log.info("Address: {}", address);
-            log.info("Price: {} 元/月", price);
-            log.info("Floor and Area: {}", floorAndArea);
-            log.info("Distance to MRT: {}", distanceToMrtName + distanceToMRT);
-            log.info("---------------------------------------");
+            RentalInfo rentalIfo = new RentalInfo();
+            rentalIfo.setTitle(title);
+            rentalIfo.setLink(link);
+            rentalIfo.setAddress(address);
+            rentalIfo.setPrice(price);
+            rentalIfo.setFloorAndArea(floorAndArea);
+            rentalIfo.setDistanceToMRT(distanceToMrtName+distanceToMRT);
+            list.add(rentalIfo);
         }
     }
 }
