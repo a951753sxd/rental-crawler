7504319ed2ce17953699c0f1c6d1a7e6ebe8be55
a951753sxd <a363636005@gmail.com>
2024-09-01 16:58:12 +0800

    java爬蟲挑戰 Day 15-建立監聽器.

--- src/main/java/tw/grass/rental_crawler/DiscordBotRunner.java
+++ src/main/java/tw/grass/rental_crawler/DiscordBotRunner.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Component;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 @Component
 public class DiscordBotRunner implements ApplicationRunner {
@@ -22,8 +25,18 @@ public class DiscordBotRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
 
         JDABuilder builder = JDABuilder.createDefault(discordToken);
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT); // 啟用 MESSAGE_CONTENT Intent
+        builder.addEventListeners(new DiscordBotListener());
         JDA jda = builder.build();
         log.info("Discord bot creation completed.");
+    }
+
+    class DiscordBotListener extends ListenerAdapter {
+        @Override
+        public void onMessageReceived(MessageReceivedEvent event) {
+            String message = event.getMessage().getContentRaw();
+            log.info("Received: {}", message);
+        }
     }
 
 }
